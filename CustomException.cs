14d5cff7daee19b248e211accf0866109aa79c02
kiranravindrant <kiranravindrant@gmail.com>
2021-07-30 23:15:01 +0530

    [kiran]Added: CustomException class to Project

---
+++ CustomException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RegexUserRegistration
+{
+    public class CustomStringException:Exception
+    {
+
+        public CustomStringException(string message) : base(message)
+        {
+
+
+
+        }
+
+
+    }
+}
