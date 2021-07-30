27219a4ca390e15a004d5d8952e1daa9bd27c1de
kiranravindrant <kiranravindrant@gmail.com>
2021-07-30 23:33:16 +0530

    [kiran]Added:Exception to Lastname Validation

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace RegexUserRegistration
         public static string namePattern = "^[A-Z]{1}[a-z]{2,}";
         public static string emailPattern = "^[A-z a-z  ]{3,}[-+0-9 ]?[.]?([a-z A-z 0-9]{3,})?[@][a-z A-z 0-9]{1,}[.]?[a-z]{2,}[.]?[a-z]{2,3}$";
         public static string mobileNumPattern = "^[9][1][ ][0-9]{10}$";
-        public static string passPattern = "^((?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*-_.])(?=.{8,}))";                                
+        public static string passPattern = "^((?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*-_.])(?=.{8,}))";
         static void Main(string[] args)
         {
             try
@@ -18,11 +18,11 @@ namespace RegexUserRegistration
                 Console.WriteLine("=======User Registration=====");
                 Console.WriteLine("Enter your FirstName");
                 firstname = Console.ReadLine();
-                if (firstname == null| firstname=="0"  )
+                if (firstname == null | firstname == "0")
                 {
                     throw new CustomStringException("cannot accept Empty or null string ");
                 }
-                else if(NameValidation(firstname) == false)
+                else if (NameValidation(firstname) == false)
                 {
                     throw new CustomStringException("Invalid format");
                 }
@@ -34,8 +34,27 @@ namespace RegexUserRegistration
 
 
             Console.WriteLine("Enter your LastName");
-            string lastname = Console.ReadLine();
-            Console.WriteLine(NameValidation(lastname));
+
+            try
+            {
+                string lastname = Console.ReadLine();
+
+
+                if (lastname == null | lastname == "0")
+                {
+                    throw new CustomStringException("cannot accept Empty or null string ");
+                }
+                else if (NameValidation(lastname) == false)
+                {
+                    throw new CustomStringException("Invalid format");
+                }
+            }
+            catch (CustomStringException e)
+            {
+                Console.WriteLine("String format Exception: {0}", e.Message);
+            }
+
+
 
             Console.WriteLine("Enter Email");
             string email = Console.ReadLine();
@@ -82,21 +101,22 @@ namespace RegexUserRegistration
         }
 
         public static bool PassValidation(string pass)
-        {   
+        {
             return Regex.IsMatch(pass, passPattern);
         }
 
 
-        //CATCH BLOCKS
-
-
-         
 
 
 
 
+
+
+
+
+    }
 }
 
-}      
+        
 
 
