e780f4d24b67c122b5ce242e6d379111d7b5684c
kiranravindrant <kiranravindrant@gmail.com>
2021-08-11 23:52:34 +0530

    [kiran]Refactored Existing with Lambda

---
+++ UserRegistrationMainWithLambda.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+
+namespace RegexUserRegistration
+{
+    public class UserRegistration
+    {
+     
+
+
+        static void Main(string[] args)
+        {
+            UserRegistrationValidation obj = new UserRegistrationValidation();
+
+            Console.WriteLine("Checking Validation of User Input : ");
+
+            obj.validateFirstName();
+
+
+
+
+
+        }
+
+
+
+    }
+
+}
+
+        
+
+
